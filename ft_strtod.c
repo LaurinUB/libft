d5b4daad7e7110e19f8df5fff526edc127022fb1
LaurinUB <Laurinub@yahoo.de>
2023-04-21 11:26:21 +0200

    update ft_strtod for negative values

--- ft_strtod.c
+++ ft_strtod.c
@@ -6,7 +6,7 @@
 /*   By: luntiet- <luntiet-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/29 17:53:03 by luntiet-          #+#    #+#             */
-/*   Updated: 2023/04/04 12:40:52 by luntiet-         ###   ########.fr       */
+/*   Updated: 2023/04/21 11:25:38 by luntiet-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,9 @@ double	ft_strtod(char *str)
 	if (!ft_strchr(str, '.'))
 		return ((double)ft_atoi(str));
 	split = ft_split(str, '.');
-	pre = (double)ft_atoi(split[0]);
-	post = (double)ft_atoi(split[1]);
+	pre = fabs((double)ft_atoi(split[0]));
+	post = fabs((double)ft_atoi(split[1]));
 	res = pre + (post / pow(10, ft_strlen(split[1])));
-	ft_free_stra(split);
 	if (str[0] == '-')
 		res = -res;
 	return (res);
